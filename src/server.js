63e413765901257d53108caa2887f43ed2ed0fbf
hiroki0h <50644511+hiroki0h@users.noreply.github.com>
2023-10-30 18:20:15 +0900

    중간저장

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 import express from 'express';
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 console.log('hello');
-app.listen(3000);
+
+const handleListen = () => console.log(`Listening on http://localhost:4000`);
+app.listen(4000, handleListen);
