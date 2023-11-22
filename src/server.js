468a35eea4125049e591437e85c5c3a3cd9bc787
hiroki0h <50644511+hiroki0h@users.noreply.github.com>
2023-11-22 16:55:07 +0900

    page redirect

--- src/server.js
+++ src/server.js
@@ -8,5 +8,6 @@ app.use('/public', express.static(__dirname + '/public'));
 console.log('hello');
 
 app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
 const handleListen = () => console.log(`Listening on http://localhost:4000`);
 app.listen(4000, handleListen);
