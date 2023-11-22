c19e79d64ee81cbe569701d538ff569ceb3c8754
hiroki0h <50644511+hiroki0h@users.noreply.github.com>
2023-11-22 16:47:12 +0900

    pug 파일 랜더링 셋팅

--- src/server.js
+++ src/server.js
@@ -6,5 +6,6 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 console.log('hello');
 
+app.get('/', (req, res) => res.render('home'));
 const handleListen = () => console.log(`Listening on http://localhost:4000`);
 app.listen(4000, handleListen);
